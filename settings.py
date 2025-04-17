5d48417b329a1376e35591ad16045bde8a8bb8e4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-17 12:20:54 +0530

    settings: HOME_DIR, ROOT_DIR, LINKEDIN_API_USER_DIR, COOKIE_PATH

---
+++ settings.py
@@ -0,0 +1,13 @@
+'''
+@author: Krisna Pranav
+@filename: settings.py
+@date: 17 April 2025
+'''
+
+import os
+from pathlib import Path
+
+HOME_DIR = str(Path.home())
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+LINKEDIN_API_USER_DIR = os.path.join(HOME_DIR, ".linkedin_api/")
+COOKIE_PATH = os.path.join(LINKEDIN_API_USER_DIR, "cookies/")
